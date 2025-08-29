4f834af768bbfb924482bafd6c3e06f071e330f2
Omkarthorat1042 <omkarthorat1042@gmail.com>
2025-08-29 17:02:17 +0530

    created some changes for dockers

--- Backend/Travyway/src/main/java/com/travyway/feignclientservices/ContentServiceClient.java
+++ Backend/Travyway/src/main/java/com/travyway/feignclientservices/ContentServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ContentService")
+@FeignClient(name = "contentservice")
 public interface ContentServiceClient {
 
     @GetMapping("/api/content/places/{id}")
